a89bb9d7045dc16632238988583270453300fc6c
Josh Ferriday <70819699+jferriday@users.noreply.github.com>
2021-06-02 17:27:21 +0100

    implement CORS middleware for development server

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const jsonParser = express.json();
 const morgan = require('morgan');
+const cors = require('cors');
+
+
 
 // import routers
 const nameSearchRouter = require('./routes/nameSearch');
 const redlistRouter = require('./routes/redlist');
+
+// CORS origin settings
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // parse request body as JSON
 app.use(morgan('dev'))
